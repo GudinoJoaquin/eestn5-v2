5691b90f8128ad1cda54f491a02c76ae6151db54
GudinoJoaquin <130807340+GudinoJoaquin@users.noreply.github.com>
2024-12-06 16:34:59 -0300

    Add files via upload

--- app/page.tsx
+++ app/page.tsx
@@ -3,17 +3,29 @@ import background from "./assets/img/frente-escuela.webp";
 import { montserrat } from "./ui/fonts";
 
 import ButtonCarrusel from "./components/ButtonCarrusel";
+<<<<<<< HEAD
+import NewsCarrusel from "./components/NewsCarrusel";
+import Temporal from "./components/TemporalCarrusel";
+
+=======
 import NoticeCarrusel from "./components/NewsCarrusel";
 import Temporal from "./components/TemporalCarrusel";
+>>>>>>> 9bef3aef9fddc72fa19662b1a60b17dd6332978a
 
 export default function Home() {
   return (
     <>
+<<<<<<< HEAD
+      <main
+        className={`text-white flex flex-col items-center overflow-x-hidden background`}
+      >
+=======
       <main className="text-white flex flex-col items-center overflow-x-hidden">
+>>>>>>> 9bef3aef9fddc72fa19662b1a60b17dd6332978a
         <Image
           src={background}
           alt="Fondo"
-          layout="fill" // Ocupa todo el espacio del contenedor
+          layout="fill" // Ocupa todo el espacio del contenedorz
           objectFit="cover" // Ajusta la imagen al tamaño del contenedor
           priority
           className="-z-10 brightness-[30%]" // Envía la imagen al fondo
@@ -34,7 +46,11 @@ export default function Home() {
           <Temporal />
         </section>
         <section className="mt-[10px]">
+<<<<<<< HEAD
+          <NewsCarrusel />
+=======
           <NoticeCarrusel />
+>>>>>>> 9bef3aef9fddc72fa19662b1a60b17dd6332978a
         </section>
         <section className="mt-[20px]">
           <ButtonCarrusel />
--- app/utils/utils.ts
+++ app/utils/utils.ts
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import anuncios from "../assets/img/anuncios.svg";
 import contacto from "../assets/img/contacto.svg";
 import especialidades from "../assets/img/especialidades.svg";
@@ -12,4 +13,20 @@ export const HomeButtonController = [
 
 export const NoticeContent = [
     "Ingreso 2025", "Mesas julio", "Equivalencias diciembre"
+=======
+import anuncios from "../assets/img/anuncios.svg";
+import contacto from "../assets/img/contacto.svg";
+import especialidades from "../assets/img/especialidades.svg";
+import nosotros from "../assets/img/nosotros.svg";
+
+export const HomeButtonController = [
+    {text: "Nuestro Contacto", img: contacto, href: "#"},
+    {text: "Ver Anuncios", img: anuncios, href: "#"},
+    {text: "Sobre Nosotros", img: nosotros, href: "#"},
+    {text: "Nuestras Especialidades", img: especialidades, href: "#"},
+]
+
+export const NoticeContent = [
+    "Ingreso 2025", "Mesas julio", "Equivalencias diciembre"
+>>>>>>> 9bef3aef9fddc72fa19662b1a60b17dd6332978a
 ]
